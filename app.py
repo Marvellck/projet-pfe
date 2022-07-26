23d05ed73d4e2735fe2cabcdfe6e9067ab795bcc
Marvel <urielmarvel@yahoo.fr>
2022-07-26 14:28:15 +0200

    new-1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
-from flask import Flask , render_template,request,redirect,url_for,jsonify,os
+from flask import Flask , render_template,request,redirect,url_for,jsonify
 from flask_pymongo import PyMongo
+import os
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://Marvel:Password123@cluster0.4eo8ulb.mongodb.net/test?retryWrites=true&w=majority"
